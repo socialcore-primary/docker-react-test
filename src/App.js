2e8690b8d8b2f86293b07e3c42a26b8693077bd4
Om Misra <ommisra@Oms-MacBook-Pro.local>
2019-11-05 22:33:14 +0530

    added the p tag again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SDC <code>src/App.js</code> I was changed on the feature branch
+          SDC <code>src/App.js</code> I ws changed on the feature branch
+          
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
